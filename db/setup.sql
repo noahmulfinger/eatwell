f832cd481b99c199b645c9802f276cb85b861269
Shinjini Nunna <sjvnunna@gmail.com>
2015-12-16 09:04:08 -0800

    Updated setup.sql

--- db/setup.sql
+++ db/setup.sql
@@ -83,6 +83,20 @@ INSERT INTO User VALUES (0,'Shinjini','sjvnunna@gmail.com','pbkdf2:sha1:1000$XWp
 -- Insert food items
 INSERT INTO Food_Item VALUES (0, 'Hamburger');
 INSERT INTO Food_Item VALUES (0, 'Chicken Pasta');
+INSERT INTO Food_Item VALUES (0, 'Apple Pie');
+INSERT INTO Food_Item VALUES (0, 'French Toast');
+INSERT INTO Food_Item VALUES (0, 'Chicken Piccata');
+INSERT INTO Food_Item VALUES (0, 'Chicken Tacos');
+INSERT INTO Food_Item VALUES (0, 'Chicken Pasta');
+INSERT INTO Food_Item VALUES (0, 'Roast Beef');
+INSERT INTO Food_Item VALUES (0, 'Lasagna');
+INSERT INTO Food_Item VALUES (0, 'Quesadillas');
+INSERT INTO Food_Item VALUES (0, 'Chocolate Chip Cookie');
+INSERT INTO Food_Item VALUES (0, 'Cheesecake');
+INSERT INTO Food_Item VALUES (0, 'Eggs Benedict');
+INSERT INTO Food_Item VALUES (0, 'Chocolate Walnut Fudge');
+INSERT INTO Food_Item VALUES (0, 'Tilapia');
+INSERT INTO Food_Item VALUES (0, 'Pork Chops');
 
 -- Insert into ingredients
 INSERT INTO Ingredient VALUES (0, 'Lettuce');
@@ -101,14 +115,35 @@ INSERT INTO Ingredient VALUES (0, 'Feta Cheese');
 INSERT INTO Ingredient VALUES (0, 'Parsley');
 INSERT INTO Ingredient VALUES (0, 'Salt');
 INSERT INTO Ingredient VALUES (0, 'Pepper');
+INSERT INTO Ingredient VALUES (0, 'Egg');
+INSERT INTO Ingredient VALUES (0, 'Cheese');
+INSERT INTO Ingredient VALUES (0, 'Milk');
+INSERT INTO Ingredient VALUES (0, 'Apple');
+INSERT INTO Ingredient VALUES (0, 'Strawberries');
+INSERT INTO Ingredient VALUES (0, 'Tomatoes');
+INSERT INTO Ingredient VALUES (0, 'Chocolate');
+INSERT INTO Ingredient VALUES (0, 'Wine');
+INSERT INTO Ingredient VALUES (0, 'Bananas');
+INSERT INTO Ingredient VALUES (0, 'Avacado');
+
 
 -- Insert into Symptoms
 INSERT INTO Symptom VALUES (0, 'Headache');
 INSERT INTO Symptom VALUES (0, 'Nausea');
+INSERT INTO Symptom VALUES (0, 'Bloating');
+INSERT INTO Symptom VALUES (0, 'Migraine');
+INSERT INTO Symptom VALUES (0, 'Cough');
+INSERT INTO Symptom VALUES (0, 'Stomach ache');
+INSERT INTO Symptom VALUES (0, 'Hives');
+INSERT INTO Symptom VALUES (0, 'Irritable bowel');
 
 -- Insert into Badges
 INSERT INTO Badge VALUES (0, 'Gluten');
 INSERT INTO Badge VALUES (0, 'Lactose');
+INSERT INTO Badge VALUES (0, 'Fructose');
+INSERT INTO Badge VALUES (0, 'Salicylates');
+INSERT INTO Badge VALUES (0, 'Amines');
+-- Insert into Eats
 
 -- Insert into Eats
 INSERT INTO Eats VALUES (1, 1, '2015-11-15 18:46:29');
@@ -116,9 +151,9 @@ INSERT INTO Eats VALUES (1, 2, '2015-11-16 18:46:29');
 INSERT INTO Eats VALUES (2, 2, '2015-11-17 18:46:29');
 
 -- Insert into Has
-INSERT INTO Has VALUES (1, 1, 'low', '2015-11-15 19:46:29');
-INSERT INTO Has VALUES (2, 1, 'moderate', '2015-11-16 19:46:29');
-INSERT INTO Has VALUES (2, 2, 'severe', '2015-11-17 19:46:29');
+INSERT INTO Has VALUES (1, 1, 'Mild', '2015-11-15 19:46:29');
+INSERT INTO Has VALUES (2, 1, 'Moderate', '2015-11-16 19:46:29');
+INSERT INTO Has VALUES (2, 2, 'Severe', '2015-11-17 19:46:29');
 
 -- Insert into Contains
 INSERT INTO Contains VALUES (1, 1);
